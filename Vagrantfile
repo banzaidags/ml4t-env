4eb3a41bb325adb85a66c33d6d5ca33bd5f26e08
banzaidags <52186863+banzaidags@users.noreply.github.com>
2021-03-18 21:19:45 -0700

    Initial upload

---
+++ Vagrantfile
@@ -0,0 +1,17 @@
+#VAGRANT_EXPERIMENTAL="cloud_init"
+
+Vagrant.configure("2") do |config|
+
+  config.vm.define "ml4t" do |ml4t|
+    ml4t.vm.hostname = "ml4t"
+    #ml4t.vm.box = "ubuntu/trusty64"
+    ml4t.vm.box = "ubuntu/bionic64"
+    ml4t.vm.provision "shell", path: "provision.sh"
+    ml4t.vm.provision "file", source: "env.yml", destination: "/tmp/env.yml"
+    ml4t.vm.provision "shell", inline: "mv /tmp/env.yml /home/ml4t/env.yml"
+    ml4t.vm.provider "virtualbox" do |vb|
+      vb.memory = "2048"
+      vb.cpus = 2
+    end
+  end
+end
---
+++ provision.sh
@@ -0,0 +1,22 @@
+sudo apt-get update
+
+# Install python3
+#sudo apt-get -y install python3 python3-pip python3-apt
+#sudo useradd -m -p $(openssl passwd -1 student) ml4t
+
+## ANACONDA INSTALL ##
+# Go to home directory
+cd ~
+
+# You can change what anaconda version you want at 
+# https://repo.continuum.io/archive/
+wget -q https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh --progress=bar:force:noscroll
+bash Anaconda3-2020.11-Linux-x86_64.sh -b -p ~/anaconda
+rm Anaconda3-2020.11-Linux-x86_64.sh
+echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc
+#echo 'export PATH="/root/anaconda/bin:$PATH"' >> /etc/bash.bashrc
+
+# Reload default profile
+source ~/.bashrc
+
+~/anaconda/bin/conda update conda -y
